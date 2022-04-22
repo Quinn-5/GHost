f5de0cd16de9e25043408afadd74930fa372936c
Quinlan Duval <quinlan.duval@comcast.net>
2022-04-22 18:32:54 -0400

    added html template

--- webserver.go
+++ webserver.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type Page struct {
+	Title string
+	Text  string
+}
+
 func index_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is my first web server in Go!")
+	p := Page{Title: "This is a templated site", Text: "This is pretty neat"}
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, p)
 }
 
 func about_handler(w http.ResponseWriter, r *http.Request) {
