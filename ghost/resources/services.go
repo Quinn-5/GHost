1d9f75b4df717b7dc7c448081221c42c3c39ccf0
Quinlan Duval <quinlan.duval@comcast.net>
2022-06-03 23:27:38 -0400

    added services file

---
+++ ghost/resources/services.go
@@ -0,0 +1,40 @@
+package resources
+
+import (
+	"context"
+	"fmt"
+
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func CreateNodeport(clientset *kubernetes.Clientset, name string, internalPort int32, protocol apiv1.Protocol) {
+	servicesClient := clientset.CoreV1().Services(apiv1.NamespaceDefault)
+
+	nodeport := &apiv1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Spec: apiv1.ServiceSpec{
+			Selector: map[string]string{
+				"app": name,
+			},
+			Ports: []apiv1.ServicePort{
+				{
+					Port:     internalPort,
+					Protocol: protocol,
+				},
+			},
+			Type: apiv1.ServiceTypeNodePort,
+		},
+	}
+
+	fmt.Println("Creating NodePort...")
+	result, err := servicesClient.Create(context.TODO(), nodeport, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created NodePort %q.\n", result.GetObjectMeta().GetName())
+
+}
