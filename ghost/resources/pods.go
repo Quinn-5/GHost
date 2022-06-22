576402707c2511a3efdf4a74ec00265294b8f5da
Quinlan Duval <quinlan.duval@comcast.net>
2022-06-21 20:50:55 -0400

    Added interface to get pod by servconf

---
+++ ghost/resources/pods.go
@@ -0,0 +1,24 @@
+package resources
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Quinn-5/learning-go/ghost/servconf"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func GetPod(config *servconf.ServerConfig) *apiv1.Pod {
+	podsClient := config.GetKubeConfig().CoreV1().Pods(apiv1.NamespaceDefault)
+
+	fmt.Println("Getting Pod...")
+	pod, err := podsClient.Get(context.TODO(), config.Servername, metav1.GetOptions{})
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("Found Pod %q.\n", config.Servername)
+	}
+
+	return pod
+}
